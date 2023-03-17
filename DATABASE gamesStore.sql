4a63e377cc136de926364c622dea0673ef0864ba
Mateo <mattt7981@gmail.com>
2023-03-16 22:06:18 -0500

    correccion procedimientos

--- DATABASE gamesStore.sql
+++ DATABASE gamesStore.sql
@@ -55,7 +55,7 @@ CREATE PROCEDURE informacion_asesor_cliente (@id_cliente INT)
 AS
 begin	
 SELECT cliente.id_cliente,cliente.nombre,asesor.id_asesor,asesor.nombre,asesor.id_tienda FROM asesor,cliente
-WHERE @id_cliente = cliente.id_cliente AND asesor.id_asesor=cliente.id_asesor 
+WHERE cliente.id_cliente = @id_cliente AND asesor.id_asesor=cliente.id_asesor 
 END
 
 
@@ -73,20 +73,20 @@ begin
 SELECT cliente_producto.id_cliente_producto,cliente.nombre,producto.id_producto,producto.nombre,producto.categoria,producto.precio,
 SUM(SUM(producto.precio)) over (order by cliente_producto.id_cliente_producto ASC) AS total_acumulado 
 FROM cliente,producto,cliente_producto
-WHERE @id_producto=producto.id_producto AND producto.id_producto=cliente_producto.id_producto AND cliente.id_cliente=cliente_producto.id_cliente
+WHERE producto.id_producto = @id_producto AND producto.id_producto=cliente_producto.id_producto AND cliente.id_cliente=cliente_producto.id_cliente
 GROUP BY cliente_producto.id_cliente_producto,cliente.nombre,producto.id_producto,producto.nombre,producto.categoria,producto.precio
 END
 
-CREATE PROCEDURE actualizar_edadd (@id_asesor INT,@edad INT)
+CREATE PROCEDURE actualizar_edad (@id_asesor INT,@edad INT)
 AS
 begin	
-UPDATE asesor SET @edad = asesor.edad
-WHERE @id_asesor = asesor.id_asesor
+UPDATE asesor SET asesor.edad = @edad
+WHERE asesor.id_asesor = @id_asesor
 END
 
 CREATE PROCEDURE eliminar_juego (@id_producto INT)
 AS
 begin	
 DELETE FROM producto
-WHERE @id_producto = producto.id_producto
+WHERE producto.id_producto = @id_producto 
 END
